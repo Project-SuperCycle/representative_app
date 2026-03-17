a6c1033efab2c3dff3aaf7d9132883fa37c1d4b4
EsoElzantot <eslamelzantot@gmail.com>
2026-03-17 23:17:29 +0200

    feat(representative_profile): handle profile loading indicator

---
+++ lib/features/representative_main_profile/presentation/widgets/loading/shipments_history_loading_indicator.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShipmentsHistoryLoadingIndicator extends StatelessWidget {
+  const ShipmentsHistoryLoadingIndicator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
